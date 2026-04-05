28972766eacd9b752fda91f2861e35d9a37985e7
RushikeshAkolkar <rushikeshakolkar150@gmail.com>
2026-04-05 18:59:46 +0530

    All Numbers Defination is coded in PrimeNumberRange.java

--- PrimeNumberRange.java
+++ PrimeNumberRange.java
@@ -4,13 +4,25 @@
 prime number -: the numbers which are divisible by 0 and it self or
                 the numbers which have only 2 factores.
 composite Number -: the Numbers which have more than 2 factores is 
-                    called as composite Number
+                    called as composite Number e.g 6->2,3,1,6
 Perfect Number -: the number is a positive integer equal to the 
                 sum of its proper positive divisors .
                 e.g- the first 4 perfect numbers are
                 6,28,496,8128
+even Number -: the Number which are divisible by 2 or reminder is 0 
+              then that number is called as even Number
+odd number -: the number not divisible by 2 or reminder is 1
+                then that Number is called as odd number.
+natural number -: the number which is ...-3,-2,-1,0,1,2,3....
+                    then that Number is called as natural Number
+whole Number  -:  0,1,2,3,.... this series is called as whole number
+negative number -: -1,-2,-3,..... this series is called as negative number
+positive number -:1,2,3,.... this series is called as positive number.
+complex number -: sqrt(3)/2  this type of number is called as complex number
 
 */
+
+
 /*
 i/p -: how many Array Element you want in an Array
 10000       
